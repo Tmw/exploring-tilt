bf6b196b7777f2eccb0557cad84d6be1ce205bf1
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2026-06-01 21:51:37 +0200

    Small cleanup

--- cmd/todo-api/main.go
+++ cmd/todo-api/main.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"cmp"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
 
-	todosservice "github.com/tmw/exploring-tilt/internal/todo-service"
-	"github.com/tmw/exploring-tilt/pkg/httphelper"
+	todosservice "github.com/tmw/exploring-tilt/internal/todo"
+	todoapi "github.com/tmw/exploring-tilt/internal/todo-api"
 )
 
 const (
@@ -32,7 +30,6 @@ func getConfig() config {
 }
 
 func main() {
-	mux := http.NewServeMux()
 	config := getConfig()
 	persistance := todosservice.NewFilePersitanceWithPath(config.StoragePath)
 	svc, err := todosservice.NewWithPersistance(persistance)
@@ -40,58 +37,20 @@ func main() {
 		slog.Error("error setting up service", slog.Any("error", err))
 		os.Exit(1)
 	}
-
-	mux.HandleFunc("GET /todos", func(w http.ResponseWriter, r *http.Request) {
-		todos := svc.List()
-		json.NewEncoder(w).Encode(todos)
-	})
-
-	mux.HandleFunc("POST /todos", func(w http.ResponseWriter, r *http.Request) {
-		var params todosservice.CreateTodoParams
-		if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
-			slog.Warn("error decoding JSON body", slog.Any("error", err))
-			return
-		}
-
-		todos := svc.Create(params)
-		json.NewEncoder(w).Encode(todos)
-	})
-
-	mux.HandleFunc("DELETE /todos/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		todos, err := svc.Delete(id)
-
-		if err != nil {
-			if errors.Is(err, todosservice.ErrItemNotFound) {
-				httphelper.RespondError(w, 404, "item not found")
-				return
-			}
-
-			slog.Error("error deleting todo", slog.Any("error", err))
-			httphelper.RespondError(w, 500, "internal server error")
-			return
-		}
-
-		json.NewEncoder(w).Encode(todos)
-	})
-
-	mux.HandleFunc("GET /version", func(w http.ResponseWriter, r *http.Request) {
-		version := "0.0.2"
-		fmt.Fprintf(w, "{\"version\": \"%s\"}", version)
-	})
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	api := todoapi.New(svc, logger)
 
 	server := http.Server{
 		Addr:    fmt.Sprintf(":%s", config.ServerPort),
-		Handler: mux,
+		Handler: api.Mux(),
 
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  10 * time.Second,
 	}
 
-	slog.Info("server started", slog.String("addr", server.Addr))
-
+	logger.Info("server started", slog.String("addr", server.Addr))
 	if err := server.ListenAndServe(); err != nil {
-		slog.Error("error while binding server", slog.Any("error", err))
+		logger.Error("error while binding server", slog.Any("error", err))
 	}
 }
---
+++ internal/todo-api/api.go
@@ -0,0 +1,73 @@
+package todoapi
+
+import (
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"net/http"
+
+	todo "github.com/tmw/exploring-tilt/internal/todo"
+	"github.com/tmw/exploring-tilt/pkg/httphelper"
+)
+
+type Service interface {
+	Create(params todo.CreateTodoParams) []todo.Todo
+	List() []todo.Todo
+	Delete(id string) ([]todo.Todo, error)
+}
+
+type Api struct {
+	svc    Service
+	logger *slog.Logger
+}
+
+func New(svc Service, logger *slog.Logger) *Api {
+	return &Api{
+		svc:    svc,
+		logger: logger,
+	}
+}
+
+func (a *Api) HandleGetTodos(w http.ResponseWriter, r *http.Request) {
+	todos := a.svc.List()
+	json.NewEncoder(w).Encode(todos)
+}
+
+func (a *Api) HandleCreateTodo(w http.ResponseWriter, r *http.Request) {
+	var params todo.CreateTodoParams
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		a.logger.Warn("error decoding JSON body", slog.Any("error", err))
+		return
+	}
+
+	todos := a.svc.Create(params)
+	json.NewEncoder(w).Encode(todos)
+}
+
+func (a *Api) HandleDeleteTodo(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	todos, err := a.svc.Delete(id)
+
+	if err != nil {
+		if errors.Is(err, todo.ErrItemNotFound) {
+			httphelper.RespondError(w, 404, "item not found")
+			return
+		}
+
+		a.logger.Error("error deleting todo", slog.Any("error", err))
+		httphelper.RespondError(w, 500, "internal server error")
+		return
+	}
+
+	json.NewEncoder(w).Encode(todos)
+}
+
+func (a *Api) Mux() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /todos", a.HandleGetTodos)
+	mux.HandleFunc("POST /todos", a.HandleCreateTodo)
+	mux.HandleFunc("DELETE /todos/{id}", a.HandleDeleteTodo)
+
+	return mux
+}
